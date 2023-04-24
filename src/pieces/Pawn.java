6581b87a39381eee1fcebb56ca35782056ba0f1a
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-04-24 14:23:50 -0500

    checkMove() for pawn rough draft

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -13,6 +13,7 @@ public class Pawn extends GamePiece
         super("\u265F", color, col, row);
         this.firstMoveMade = false;
         this.DIR = direction;
+
         if (!(DIR.equals("up") || DIR.equals("down") || DIR.equals("left") || DIR.equals("right")))
         {
             throw new IllegalArgumentException("`" + DIR + "`" + " is not a valid direction.");
@@ -64,5 +65,48 @@ public class Pawn extends GamePiece
         }
 
         return ArrayUtils.convert2DArrayList(moves);
+    }
+
+    @Override
+    public boolean checkMove(int x, int y, Board board)
+    {
+        int[] pos = {super.getCol(), super.getRow()};
+
+        // Initialize all possible pawn movements
+        int[][] baseMovements = new int[4][2];
+        for (int i = 0; i < baseMovements.length; i++)
+        {
+            baseMovements[i][0] = pos[0];
+            baseMovements[i][1] = pos[1];
+        }
+
+        // Adjust values accorsding to direction
+        int axis = DIR.equals("up") || DIR.equals("down")  ? 1 : 0;
+        int dir = DIR.equals("up") || DIR.equals("right") ? 1 : -1;
+        int anti = Math.abs(axis-1);
+        baseMovements[0][axis] += 1*dir;
+        baseMovements[1][axis] += 2*dir;
+        baseMovements[2][axis] += 1*dir;
+        baseMovements[2][anti] += 1;
+        baseMovements[3][axis] += 1*dir;
+        baseMovements[3][anti] -= 1;
+        if (firstMoveMade) { baseMovements[1] = null; } // edge case
+
+        // Check conditions and continue until match reaced or list exhausted
+        for (int[] move : baseMovements)
+        {
+            if (move == null) { continue; }
+
+            int spaceStatus = board.checkSpace(pos[0], pos[1], super.getColor());
+            boolean validAttack = (move[0] != pos[0]) && (spaceStatus == 2);
+            boolean validForward = (move[axis] == pos[axis] && move[anti] == pos[anti]) && (spaceStatus == 0);
+
+            if ( (validAttack || validForward) && (move[0] == x && move[1] == y) )
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
