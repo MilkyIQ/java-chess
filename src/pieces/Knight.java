aa4f537b658dc05b611f7fb7a24a74608153ab2c
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-05-02 15:13:16 -0500

    implement Knight checkMove()

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -5,12 +5,16 @@ public class Knight extends GamePiece
 {
     public Knight(String color, int col, int row)
     {
-        super("\u265D", color, col, row);
+        super("\u265E", color, col, row);
     }
 
     @Override
     public boolean checkMove(int x, int y, Board board)
     {
-        return true;
+        int deltaX = Math.abs(x - super.getCol());
+        int deltaY = Math.abs(y - super.getRow());
+        boolean longEdgeX = deltaX == 1 && deltaY == 2;
+        boolean longEdgeY = deltaX == 2 && deltaY == 1;
+        return (longEdgeX || longEdgeY) ? true : false;
     }
 }
