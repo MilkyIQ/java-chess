276a80470929ae64b7293491145348167b322c9f
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-05-06 18:08:57 -0500

    implement basic checkMove() for King

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -11,6 +11,13 @@ public class King extends GamePiece
     @Override
     public boolean checkMove(int x, int y, Board board)
     {
-        return true;
+        int deltaX = Math.abs(x - super.getCol());
+        int deltaY = Math.abs(y - super.getRow());
+
+        boolean invalidMove = deltaX > 1 || deltaY > 1;
+        boolean spaceFriendly = board.checkSpace(x, y, super.getColor()) == 1;
+        if (invalidMove || spaceFriendly) { return false; }
+
+        return true; 
     }
 }
