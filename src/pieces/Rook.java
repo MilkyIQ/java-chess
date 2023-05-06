24db07732a4502dded1784fc457d7c6e500bcd36
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-05-06 17:24:28 -0500

    start work on checkAllValidMoves() with rook

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -1,5 +1,7 @@
 package pieces;
 import game.Board;
+import java.util.ArrayList;
+import tools.ArrayUtils;
 
 public class Rook extends GamePiece
 {
@@ -35,5 +37,57 @@ public class Rook extends GamePiece
         }
         
         return true;
+    }
+
+    // Returns a 2D array of all points on board that the rook can make
+    /*
+     * Uses 4 for loops to achieve with almost identical code in each to achieve this by looping through each axis in each direction.
+     * I REALLY didnt want this one to work, but it's ~12x faster than anything else I've attempted to come up with.
+     * So for now, this is the best unoptimizied method.
+     */
+    public int[][] getAllValidMoves(Board board)
+    {
+        ArrayList<ArrayList<Integer>> moves = new ArrayList<ArrayList<Integer>>();
+        int pieceX = super.getCol();
+        int pieceY = super.getRow();
+        String color = super.getColor();
+
+        // Check x axis left of piece
+        for (int x = pieceX-1; x >= 0; x--)
+        {
+            int spaceStatus = board.checkSpace(x, pieceY, color);
+            if (spaceStatus == 1) { break; }
+            moves.add(ArrayUtils.createPoint(x, pieceY));
+            if (spaceStatus == 2) { break; }
+        }
+
+        // Check x axis right of piece
+        for (int x = pieceX+1; x < board.getLength(); x++)
+        {
+            int spaceStatus = board.checkSpace(x, pieceY, color);
+            if (spaceStatus == 1) { break; }
+            moves.add(ArrayUtils.createPoint(x, pieceY));
+            if (spaceStatus == 2) { break; }
+        }
+
+        // Check y axis south of piece
+        for (int y = pieceY-1; y >= 0; y--)
+        {
+            int spaceStatus = board.checkSpace(pieceX, y, color);
+            if (spaceStatus == 1) { break; }
+            moves.add(ArrayUtils.createPoint(pieceX, y));
+            if (spaceStatus == 2) { break; }
+        }
+
+        // Check y axis north of piece
+        for (int y = pieceY+1; y < board.getHeight(); y++)
+        {
+            int spaceStatus = board.checkSpace(pieceX, y, color);
+            if (spaceStatus == 1) { break; }
+            moves.add(ArrayUtils.createPoint(pieceX, y));
+            if (spaceStatus == 2) { break; }
+        }
+
+        return ArrayUtils.convert2DArrayList(moves);
     }
 }
