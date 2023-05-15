bb79a99768729b1f00b0c21c420f8baa45b472f7
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-05-15 15:14:17 -0500

    add validMoves function for King

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -1,5 +1,6 @@
 package pieces;
 import game.Board;
+import java.util.ArrayList;
 
 public class King extends GamePiece
 {
@@ -20,4 +21,27 @@ public class King extends GamePiece
 
         return true; 
     }
+
+    @Override
+    public void updateValidMoves(Board board, ArrayList<GamePiece> moves)
+    {
+        for (int dx = -1; dx <= 1; dx++)
+        {
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                int x = super.getCol() + dx;
+                int y = super.getRow() + dy;
+                boolean spaceIsAdjacent = dx != 0 || dy != 0;
+                boolean spaceIsFriendly = board.checkSpace(x, y, super.getColor()) == 1;
+                if (spaceIsAdjacent && !spaceIsFriendly) { moves.add(new GamePiece("x", x, y)); }
+            }
+        }
+    }
+
+    /*
+     * PF3
+         PE6
+       PG4
+         QH4
+     */
 }
