31112e603e62845d59129fcf66c2081b2f4b2d2f
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-05-14 22:59:06 -0500

    introduce chatgpt changes to Knight checkMove()

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -14,9 +14,9 @@ public class Knight extends GamePiece
     {
         int deltaX = Math.abs(x - super.getCol());
         int deltaY = Math.abs(y - super.getRow());
-        boolean longEdgeX = deltaX == 1 && deltaY == 2;
-        boolean longEdgeY = deltaX == 2 && deltaY == 1;
-        return (longEdgeX || longEdgeY) ? true : false;
+        boolean isLShapeX = deltaX == 1 && deltaY == 2;
+        boolean isLShapeY = deltaX == 2 && deltaY == 1;
+        return isLShapeX || isLShapeY;
     }
 
     @Override
@@ -33,8 +33,8 @@ public class Knight extends GamePiece
             {
                 int[] lShapeXPos = {COL+longEdgeInc, ROW+shortEdgeInc};
                 int[] lShapeYPos = {COL+shortEdgeInc, ROW+longEdgeInc};
-                boolean lShapeXInBounds = board.coordinateOutOfBounds(lShapeXPos[0], lShapeXPos[1]);
-                boolean lShapeYInBounds = board.coordinateOutOfBounds(lShapeYPos[0], lShapeYPos[1]);
+                boolean lShapeXInBounds = !board.coordinateOutOfBounds(lShapeXPos[0], lShapeXPos[1]);
+                boolean lShapeYInBounds = !board.coordinateOutOfBounds(lShapeYPos[0], lShapeYPos[1]);
                 if (lShapeXInBounds) { moves.add(new GamePiece("x", lShapeXPos[0], lShapeXPos[1])); }
                 if (lShapeYInBounds) { moves.add(new GamePiece("x", lShapeYPos[0], lShapeYPos[1])); }
             }
