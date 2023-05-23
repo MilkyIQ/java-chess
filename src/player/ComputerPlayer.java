d235098145caec0174863f8ad84963f26f13bab5
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2023-05-23 15:14:47 -0500

    add some flair

--- src/player/ComputerPlayer.java
+++ src/player/ComputerPlayer.java
@@ -17,24 +17,36 @@ public class ComputerPlayer extends Player
     @Override
     public Move selectMove(Board board)
     {
+        System.out.println("Computer is thinking...");
+        System.out.println("Simulating moves...");
+        System.out.println("Contacting parallel universes...");
+
+        Move selectedMove = null;
         switch (difficultyLevel)
         {
             case 0: 
-                return randomSelect(board);
-            default:
-                return null;
+                selectedMove = randomSelect(board);
         }
+
+        System.out.println("Move chosen!");
+        return selectedMove;
     }
 
     private Move randomSelect(Board board)
+    {
+        ArrayList<Move> allMoves = getAllPossibleMoves(board);
+        int randomIndex = (int) Math.random() * allMoves.size();
+        Move selectedMove = allMoves.get(randomIndex);
+        return selectedMove;
+    }
+
+    private ArrayList<Move> getAllPossibleMoves(Board board)
     {
         ArrayList<Move> allMoves = new ArrayList<Move>();
         for (GamePiece piece : super.getAllPieces())
         {
             piece.updateValidMoves(board, allMoves);
         }
-        int randomIndex = (int) Math.random() * allMoves.size();
-        Move selectedMove = allMoves.get(randomIndex);
-        return selectedMove;
+        return allMoves;
     }
 }
